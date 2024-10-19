6eb7159823a4b907ff3b70eb629d67596a7d0a29
Giomonsef <gmons045@uottawa.ca>
2024-10-19 01:17:15 -0400

    website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // You can add custom global styles here if needed
+import Portfolio from './top'; // Import the Greeting component
+import Resume from './resume'
+import About from './contact'
+import Greeting from './greeting'
+import Contact from './email'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {/* Greeting Component */}
+          <Portfolio />
+          <About />
+          <Resume />
+          <Contact />
+
+
+
+
+      </div>
   );
 }
 
